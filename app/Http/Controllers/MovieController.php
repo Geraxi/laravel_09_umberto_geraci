cd81a9a6594073f8af1e33d27ad61d4096424b03
Umberto Geraci <umbertogeraci@Umbertos-MBP-10.Home>
2025-03-08 14:11:09 +0100

    finished

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,50 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Movie;
 
-
-
 class MovieController extends Controller
-
 {
-    public $movies =[
-        ['id'=> '1', 'title'=> 'Interstellar', 'director'=> 'Christopher Nolan', 'img'=> '/media/interstellar.png' , 'genres'=>'Sci-Fi'],
-        ['id'=> '2', 'title'=> 'Harry Potter 6', 'director'=> 'David Yates', 'img'=> '/media/hp.png', 'genres'=>'Fantasy'],
-        ['id'=> '3', 'title'=> 'The Fog', 'director'=> 'John Carpenter', 'img'=>'/media/fog.png' , 'genres'=>'Horror'],
-        ['id'=> '4', 'title'=> 'A Queit Place', 'director'=>'John Krasinski', 'img'=>'/media/aqp.png', 'genres'=>'Horror'],
-        ['id'=> '5', 'title'=> 'Red Notice' , 'director'=>'Rawson Marshall Thruber' , 'img'=>'/media/rednotice.png', 'genres'=>'Action Comedy'],
-    ];
-   public function movieList()
-   {
-     $movies=Movie::all();
-        
-        return view('movie.movies', ['movies'=>$movies]);
+    // Show the list of movies
+    public function movieList()
+    {
+        $movies = Movie::all();
+        return view('movies.index', compact('movies')); // Returns the 'movies.index' view
     }
 
-    public function movieDetail($id){
-         foreach($this->movies as $movie){
-            if($id == $movie['id']){
-                return view('movie-detail' , ['movie'=>$movie]);
-            }
+    // Show details of a single movie
+    public function movieDetail($id)
+    {
+        $movie = Movie::find($id);
+
+        if (!$movie) {
+            return redirect()->route('movies.index')->with('errorMessage', 'Movie not found.');
         }
 
+        return view('movies.show', compact('movie')); // Returns the 'movies.show' view
     }
 
-    public function create(){
-        return view('movie.create');
+    // Show the form to create a movie
+    public function create()
+    {
+        return view('movies.create');
     }
 
-
-
+    // Store new movie data
     public function store(Request $request)
-{
-    $movie=Movie::create([
-        'title'=>$request->title,
-        'director'=>$request->director,
-        'year'=>$request->year,
-        'plot'=>$request->plot
-    ]);
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'director' => 'required|string|max:255',
+            'year' => 'required|integer|min:1900|max:' . date('Y'),
+            'plot' => 'nullable|string',
+        ]);
 
-    return redirect()->route('homepage')->with('successMessage', 'Hai correttamente inserito il tuo film');
-}
+        Movie::create($request->only(['title', 'director', 'year', 'plot']));
+
+        return redirect()->route('movies.index')->with('successMessage', 'Movie added successfully!');
+    }
 }
 
